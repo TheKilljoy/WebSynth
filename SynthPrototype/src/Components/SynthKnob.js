065f0736581416ceeb5fa88f06dbf5345e664211
jlncrml <s0559811@htw-berlin.de>
2018-12-03 14:44:51 +0100

    Add knobmove Event

--- SynthPrototype/src/Components/SynthKnob.js
+++ SynthPrototype/src/Components/SynthKnob.js
@@ -1,47 +1,45 @@
 export default class SynthKnob extends HTMLElement {
-  constructor() {
-    super()
 
-    const shadowRoot = this.attachShadow({mode: 'open'}).innerHTML =
-    `
-    <style>
-    #circle {
-        position: relative;
-        width: 64px;
-        height: 64px;
-        border-radius: 50%;
-        background: #000;
-        cursor: pointer;
-    }
-    #circle::before {
-        content: '';
-        position: absolute;
-        width: 16px;
-        height: 16px;
-        margin: 8px;
-        border-radius: 50%;
-        background: #FFF;
-    }
-    </style>
-    <p>LEVEL</p>
-    <div id="circle"></div>
-    <script>
-        let input = document.querySelector('#circle')
-        input.addEventListener('mousemove', changeValue)
+    constructor() {
+        super()
 
-        function changeValue(event) {
+        const shadowRoot = this.attachShadow({mode: 'open'}).innerHTML =
+        `
+        <style>
+        #circle {
+            position: relative;
+            width: 64px;
+            height: 64px;
+            border-radius: 50%;
+            background: #000;
+            cursor: pointer;
+        }
+        #circle::before {
+            content: '';
+            position: absolute;
+            width: 16px;
+            height: 16px;
+            margin: 8px;
+            border-radius: 50%;
+            background: #FFF;
+        }
+        </style>
+        <p>LEVEL</p>
+        <div id="circle"></div>
+        `
+
+        let circle = this.shadowRoot.querySelector('#circle')
+        circle.addEventListener('mousemove', (event) => {
             let vector = [event.x, event.y]
             let centerVector = [event.target.offsetWidth / 2 - vector[0], event.target.offsetHeight / 2 - vector[1]]
             let value = Math.atan(centerVector[0], centerVector[1]) * (1 / Math.PI) + 0.5
 
-            let knobMoveEvent = document.createEvent("HTMLEvents")
-            knobMoveEvent.initEvent("knobmove", false, true)
+            console.log(value);
 
-            event.target.dispatchEvent(knobMoveEvent)
-        }
-    </script>
-    `
-  }
+            this.value = value
+            this.shadowRoot.dispatchEvent(new Event('knobmove', {bubbles: true, composed: true}))
+        })
+    }
 }
 
 customElements.define('synth-knob', SynthKnob)
